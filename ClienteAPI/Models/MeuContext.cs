45f636854c3c30fb6cb6807f48deee853d408d9f
naianaBrito <naiana.sbrito@gmail.com>
2022-02-01 21:33:11 -0300

    Initial commit

---
+++ ClienteAPI/Models/MeuContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace ClienteAPI.Models
+{
+    public class MeuContext : DbContext
+    {
+        public MeuContext() : base("name=MeuContext")
+        {
+        }
+
+        public DbSet<Usuario> Usuario { get; set; }
+    }
+}
---
+++ ClienteAPI/Models/Usuario.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ClienteAPI.Models
+{
+    public class Usuario
+    {
+        [Key]
+        public Int32 Id { get; set; }
+
+        public string Nome { get; set; }
+    }
+}
